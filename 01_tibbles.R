2f54f7c904ced34c8042fb0be4cc03d10188fbba
Heidi Steiner <steiner@pharmacy.arizona.edu>
2019-12-20 11:25:22 -0700

    new data

--- 01_tibbles.R
+++ 01_tibbles.R
@@ -9,31 +9,40 @@
 library(tidyverse)
 library(skimr)
 
-#Load data
-comicbook <- read_csv("data/comicbook.csv")
+
+dc <- read_csv("data/dc.csv")
+marvel = read_csv("data/marvel.csv")
+names(marvel)[names(marvel) == "Year"] = "YEAR"
+
+marvel$publisher = "marvel"
+dc$publisher = "dc"
+
+comicbook = full_join(dc, marvel)
 
 # View data
 comicbook %>% skim()
 
+
+
 ## plot 1 tibble 
 newchars <- comicbook %>%
-  filter(grepl("Male|Female", sex)) %>%
-  mutate(Character = gsub(" Characters", "", sex)) %>%
-  group_by(publisher, Character, year) %>%
+  filter(grepl("Male|Female", SEX)) %>%
+  mutate(Character = gsub(" Characters", "", SEX)) %>%
+  group_by(publisher, Character, YEAR) %>%
   summarise(Total = n()) %>%
   ungroup() %>%
   na.omit()
 
 ## plot 1 labeller
-publishernames = c("DC, New Earth continuity", "Marvel, Earth-616 continuity")
-names(publishernames) = c("DC", "Marvel")
+publishernames = c("dc" = "DC, New Earth continuity","marvel" = "Marvel, Earth-616 continuity")
+
 
 
 ## plot 2 tibble
 percfem <- comicbook %>%
-  filter(grepl("Male|Female", sex), year >= 1980, year <= 2011) %>%
-  mutate(Character = gsub(" Characters", "", sex)) %>%
-  group_by(publisher, year) %>%
+  filter(grepl("Male|Female", SEX), YEAR >= 1980, YEAR <= 2011) %>%
+  mutate(Character = gsub(" Characters", "", SEX)) %>%
+  group_by(publisher, YEAR) %>%
   summarise(Total = n(), female = sum(Character == "Female")) %>%
   mutate(percentfemale = female/Total) %>% 
   ungroup() %>%
@@ -41,25 +50,59 @@ percfem <- comicbook %>%
 
 
 ## plot 3 tibble
-table(comicbook$align)
-comicbook %>% count(is.na(align))
-comicbook$align = if_else(is.na(comicbook$align), "Neutral", comicbook$align)
+table(comicbook$ALIGN)
+comicbook %>% count(is.na(ALIGN))
+
+comicbook$ALIGN[comicbook$ALIGN == "Bad Characte"] = "Bad Characters"
+
+comicbook$ALIGN = as.factor(comicbook$ALIGN)
+
 
 percalign <- comicbook %>% 
-  filter(grepl("Male|Female", sex)) %>%
-  mutate(Character = gsub(" Characters", "", sex)) %>%
-  group_by(publisher, sex) %>%
-  summarise(good = sum(align == "Good Characters"), 
-            neutral = sum(align == "Neutral"), 
-            bad = sum(align == "Bad Characters")) %>%
-  mutate(percentgood = good/(good + neutral + bad),
-         percentneutral = neutral/(good +neutral + bad),
-         percentbad = bad/(good + neutral + bad)) %>% 
+  filter(grepl("Male|Female", SEX), ALIGN != "Reformed Criminals") %>%
+  group_by(publisher, SEX) %>%
+  summarise(good = sum(ALIGN == "Good Characters"), 
+            neutral = sum(ALIGN == "Neutral Characters"), 
+            bad = sum(ALIGN == "Bad Characters")) %>%
+  mutate(percentgood = (good/(good + neutral + bad))*100,
+         percentneutral = (neutral/(good +neutral + bad))*100,
+         percentbad = (bad/(good + neutral + bad))*100) %>% 
   ungroup() %>%
   na.omit() %>% 
-  gather("align", "percent", 6:8)
-percalign$sex = factor(percalign$sex, labels = c("Female", "Male"), 
-                          levels = c("Female Characters", "Male Characters"))
-percalign$align = factor(percalign$align, levels = c("percentgood", "percentneutral", "percentbad"), 
-                         labels = c( "Good", "Neutral", "Bad"))
+  gather("align", "percent", 6:8) %>% 
+  group_by(publisher, SEX) %>% 
+  mutate(label_ypos = cumsum(percent)) %>% 
+  ungroup() %>% 
+  mutate_at(8, round, 0)
 
+percalign$SEX = factor(percalign$SEX, labels = c("Male", "Female"), 
+                          levels = c("Male Characters", "Female Characters"))
+
+
+percalign$align = factor(percalign$align, levels = c("percentbad", "percentneutral", "percentgood"), 
+                         labels = c( "Bad", "Neutral", "Good"))
+
+align_annotationtext = data.frame(lab = c("Good", "Neutral", "Bad"),
+                            publisher = c("dc"),
+                            align = c("Good", "Neutral", "Bad"),
+                            percent = c(3, 59, 97),
+                            cols = c("forestgreen","gold", "brickred1"))
+
+
+dcpercent_annotationtext = data.frame(lab = c("42", "8", "50", "54%", "12%", "34%"), 
+                                      publisher = c("dc"),
+                                      align = c("Good", "Neutral", "Bad",
+                                                "Good", "Neutral", "Bad"),
+                                      percent = c(2.8, 47, 97, 4.2, 61, 96))
+
+
+marvelpercent_annotationtext = data.frame(lab = c("30", "15", "55", "48", "21", "31"), 
+                                          publisher = c("marvel"),
+                                          align = c("Good", "Neutral", "Bad",
+                                                    "Good", "Neutral", "Bad"),
+                                          percent = c(2.8, 35, 97, 2.8, 58, 97))
+
+
+publisher_annotation = data.frame(lab = c("DC", "Marvel"), 
+                                  publisher = c("dc", "marvel") ,
+                                  align = c("Good"))
--- 02_ggplot.R
+++ 02_ggplot.R
@@ -5,10 +5,12 @@
 ####     18 DEC 2019     ####
 #############################
 
+library(extrafont)
+font_import()
+
 ## plot 1
-ggplot(newchars, aes(x = year, y = Total, fill = publisher)) +
-  geom_bar(stat = "identity") +
-  facet_grid(~publisher, labeller = labeller(publisher = publishernames)) +
+ggplot(newchars, aes(x = YEAR, y = Total, fill = publisher)) +
+  geom_bar(stat = "identity", width = 1) +
   scale_x_continuous("", breaks = c(1940, 1960, 1980, 2000), labels = c("1940","'60","'80","2000")) +
   scale_y_continuous("") +
   scale_fill_manual(values = c("dodgerblue", "red2")) +
@@ -16,13 +18,14 @@ ggplot(newchars, aes(x = year, y = Total, fill = publisher)) +
   theme(legend.position = "none", text = element_text(size = 18, family = "mono"),
         panel.grid.minor = element_blank(), plot.background = element_rect(fill = "gray92", color = "white"),
         panel.grid.major = element_line(color = "gray")) +
-  ggtitle("New Comic Book Characters Introduced Per Year")
+  ggtitle("New Comic Book Characters Introduced Per Year") +
+  facet_grid(~publisher, labeller = labeller(publisher = publishernames))
 
 
 
 ## plot 2 
-ggplot(percfem, aes(year, percentfemale, color = publisher)) +
-  geom_line(size = 1.5) + 
+ggplot(percfem, aes(YEAR, percentfemale, color = publisher)) +
+  geom_line(size = 1.2) + 
   scale_x_continuous("", 
                      breaks = c(1980, 1990, 2000, 2010), 
                      labels = c("1980", "'90", "2000", "'10")) +
@@ -36,23 +39,41 @@ ggplot(percfem, aes(year, percentfemale, color = publisher)) +
         panel.grid.minor = element_blank(),
         text = element_text(size = 14, family = "mono"), 
         plot.background = element_rect(fill= "gray92", color = "white"),
-        panel.grid.major = element_line(color = "gray")) +
+        panel.grid.major = element_line(color = "gray"),
+        plot.subtitle = element_text(margin = margin(0,0,30,0))) +
   ggtitle("Comics Aren't Gaining Many Female Characters", 
           subtitle = "Percentage of new characters who are female") +
   geom_text(x = 2001,  y = .48, label = "DC", color = "dodgerblue", size = 5) +
   geom_text(x = 2001, y = .25, label = "Marvel", color = "red", size = 5)
 
 ### plot 3 
-ggplot(data = percalign, aes(sex, percent, fill = align)) +
+ggplot(data = percalign, aes(SEX, percent, fill = align)) +
   geom_col() +
-  coord_flip() +
-  scale_fill_manual(values = c("firebrick1", "gold", "forestgreen")) +
+  coord_flip(ylim = c(0,100), clip = "off") +
   facet_grid(rows = vars(publisher), switch = "both") +
-  labs(x = " ", y = " ") +
-  theme_minimal()+
-  theme(strip.text.y = element_text(angle = 180),
-        strip.placement = "outside",
-        strip.background = element_blank(),
-        legend.position = "none") +
-  ggtitle("Good Girls Gone Meh", subtitle = "Character alignment by gender")
-  
+  scale_fill_manual(values = c("firebrick1", "gold", "forestgreen")) +
+  theme(legend.position = "none",
+        strip.text.y = element_blank(), 
+        axis.ticks.x = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.text.x = element_blank(),
+        text = element_text(size = 15, family = "sans"),
+        plot.background = element_rect(fill = "gray92", color = "white"),
+        panel.grid = element_blank(),
+        panel.spacing = unit(5, "lines"),
+        plot.margin = unit(c(3,0,3,2), "cm"),
+        plot.subtitle = element_text(margin = margin(0,0,50,0))) +
+  labs(y = " ", x = " ") +
+  ggtitle("Good Girls Gone Meh", subtitle = "Character alignment by gender")+
+  geom_text(data = align_annotationtext, aes(x = 3, y = percent, 
+                                       label = lab, color = factor(align)), size = 4,
+            fontface = "bold", inherit.aes = F)+
+  geom_text(data = dcpercent_annotationtext, aes(x = c(1, 1, 1, 2, 2, 2), y = percent, 
+                                                 label = lab), color = "white") +
+  geom_text(data = marvelpercent_annotationtext, aes(x = c(1, 1, 1, 2, 2,2),
+                                                     y = percent, label = lab),
+            color = "white") +
+  geom_text(data = publisher_annotation, aes(x = c(3,3), y = c(-12, -12),
+                                             label = lab, fontface = "bold"))
+         
+
